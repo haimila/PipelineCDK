011b73a1c94dca4de0c44dcf4ccb22e5873d1bac
MitjaHaimila <mitja.haimila@gmail.com>
2020-08-28 16:03:55 +0300

    github oauth päivitetty

--- pipeline_cdk/pipeline_cdk_stack.py
+++ pipeline_cdk/pipeline_cdk_stack.py
@@ -10,24 +10,39 @@ class PipelineCdkStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        sourceaction = actions.GitHubSourceAction(
-            oauth_token=TOKENITÄHÄN,
-            output=sourceoutput,
-            owner="haimila",
-            branch="master",
-            repo=""
-
-
-        )
         pipeline = codepipeline.Pipeline(
             self, "CodePipeline",
             pipeline_name="CDKPipelineTest"
         )
+        token = core.SecretValue.secrets_manager("arn:aws:secretsmanager:ap-northeast-1:821383200340:secret:GitHubKey-LPPj2Z")
+        sourceoutput = codepipeline.Artifact()
+        sourceaction = actions.GitHubSourceAction(
+            oauth_token=token,
+            output=sourceoutput,
+            owner="haimila",
+            branch="master",
+            repo="cfpipeline",
+            action_name="SourceAction"
+        )
+        iam_capabilities = core.CfnCapabilities.NAMED_IAM
+        templatepath = codepipeline.ArtifactPath(sourceoutput, "cfpipeline.yml")
+        deployaction = actions.CloudFormationCreateUpdateStackAction(
+            admin_permissions=True,
+            stack_name="CdkPipelineStack",
+            template_path=templatepath,
+            replace_on_failure=True,
+            action_name="DeployAction",
+            capabilities=[iam_capabilities]
+        )
+
+        sourcestage = pipeline.add_stage(
+            stage_name="Source",
+            actions=[sourceaction]
+        )
 
         pipeline.add_stage(
-            stage_name="Source",
-            actions=[
-
-            ]
+            stage_name="Deploy",
+            actions=[deployaction],
+            placement={"just_after": sourcestage}
         )
 
